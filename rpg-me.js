e6f98996bd47b3b16fa260f1569d18d2c7d31f38
Tylar Sanderson <tas6466@psu.edu>
2024-12-01 17:58:45 -0500

    aeong[rf

--- rpg-me.js
+++ rpg-me.js
@@ -1,17 +1,7 @@
-/**
- * Copyright 2024 tas6466
- * @license Apache-2.0, see LICENSE for full text.
- */
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
 
-/**
- * `rpg-me`
- * 
- * @demo index.html
- * @element rpg-me
- */
 export class RpgMe extends DDDSuper(I18NMixin(LitElement)) {
 
   static get tag() {
@@ -21,21 +11,8 @@ export class RpgMe extends DDDSuper(I18NMixin(LitElement)) {
   constructor() {
     super();
     this.title = "";
-    this.t = this.t || {};
-    this.t = {
-      ...this.t,
-      title: "Title",
-    };
-    this.registerLocalization({
-      context: this,
-      localesPath:
-        new URL("./locales/rpg-me.ar.json", import.meta.url).href +
-        "/../",
-      locales: ["ar", "es", "hi", "zh"],
-    });
   }
 
-  // Lit reactive properties
   static get properties() {
     return {
       ...super.properties,
@@ -43,7 +20,6 @@ export class RpgMe extends DDDSuper(I18NMixin(LitElement)) {
     };
   }
 
-  // Lit scoped styles
   static get styles() {
     return [super.styles,
     css`
@@ -57,24 +33,15 @@ export class RpgMe extends DDDSuper(I18NMixin(LitElement)) {
         margin: var(--ddd-spacing-2);
         padding: var(--ddd-spacing-4);
       }
-      h3 span {
-        font-size: var(--rpg-me-label-font-size, var(--ddd-font-size-s));
-      }
     `];
   }
 
-  // Lit render the HTML
   render() {
     return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+    <div class="wrapper">
+    </div>`;
   }
 
-  /**
-   * haxProperties integration via file reference
-   */
   static get haxProperties() {
     return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
       .href;
