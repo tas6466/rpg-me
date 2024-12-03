00b20d027f59d68eb7d1024434e2133397918539
Tylar Sanderson <tas6466@psu.edu>
2024-12-03 12:56:41 -0500

    asdfghjkl;'

--- rpg-me.js
+++ rpg-me.js
@@ -1,6 +1,7 @@
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+import "@haxtheweb/rpg-character/rpg-character.js";
 
 export class RpgMe extends DDDSuper(I18NMixin(LitElement)) {
 
